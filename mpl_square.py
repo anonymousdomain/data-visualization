22558de6cb0b0640ac735d43e22bcb17e81fb71c
anonymousdomain <dawityitagesu@gmail.com>
2022-11-29 23:35:33 +0300

    scatter

--- mpl_square.py
+++ mpl_square.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 
-x_values = range(6)
+x_values = range(1,1000)
 y_values = [x**2 for x in x_values]
 
 plt.style.use('tableau-colorblind10')
 fig, ax = plt.subplots()
-
-ax.plot(x_values, y_values,linewidth=3)
+ax.scatter(x_values ,y_values,c=y_values,cmap=plt.cm.Blues,s=10)
+ax.axis([0,1100,0,1100000])
 ax.set_title('square NUmbers',fontsize=24)
 ax.set_xlabel('Value',fontsize=24)
 ax.set_ylabel('square NUmbers',fontsize=24)
 
 ax.tick_params('both',labelsize=14)
-plt.show()
+# plt.show()
+
+plt.savefig('mpl_sqaures_plot.png',bbox_inches='tight')
+plt.close()
