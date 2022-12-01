8dd0d864ab1ed444c3a1ead8cbf051be95203b02
anonymousdomain <dawityitagesu@gmail.com>
2022-12-01 21:30:43 +0300

    rolling 6 sides dies outcome

---
+++ plotly/die.py
@@ -0,0 +1,30 @@
+from random import randint
+from plotly.graph_objs import Layout ,Bar
+from plotly import offline
+class Die:
+    def __init__(self, die_sides=6):
+        self.die_sides = die_sides
+
+    def roll(self):
+        return randint(1, self.die_sides)
+
+
+die = Die()
+
+results = []
+frequancey = []
+for i in range(100):
+    res = die.roll()
+    results.append(res)
+for value in range(1, die.die_sides+1):
+    freq = results.count(value)
+    frequancey.append(freq)
+
+x_v=list(range(1,die.die_sides+1))
+data=[Bar(x=x_v,y=frequancey)]
+
+x_axis={'title':'Result'}
+y_axis={'title':'frequncey of Result'}
+
+layout=Layout(title='Results of rolling one D6 100 times',xaxis=x_axis,yaxis=y_axis)
+offline.plot({'data':data,'layout':layout},filename='die.html')
